2ed21b0c16cb09909e5bb82e3cb70072f723e1da
Sandis Kaģis <s20kagisand@venta.lv>
2024-03-14 14:16:01 +0200

    New: GuestUser class

---
+++ src/model/user/GuestUser.java
@@ -0,0 +1,33 @@
+package model.user;
+
+public class GuestUser {
+    //1.variables
+    private long userID;
+    private static long counter = 0;
+
+    //2.get and set
+
+    public long getUserID() {
+        return userID;
+    }
+
+    public void setUserID() {
+        this.userID = counter++;
+    }
+
+    //3.constructors
+
+    public GuestUser()
+    {
+        setUserID();
+    }
+
+    //4.toString
+
+    public String toString()
+    {
+        return "" + userID;
+    }
+
+    //5.other functions
+}
