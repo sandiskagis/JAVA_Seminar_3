430d8872aef94a3a5338d10f4d3aa75adfef09b4
Sandis Kaģis <s20kagisand@venta.lv>
2024-03-21 13:40:16 +0200

    New page class

---
+++ src/model/Page.java
@@ -0,0 +1,53 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Page {
+    //1. variables
+    private String title;
+    private String description;
+    private ArrayList<Post> publicPosts = new ArrayList<Post>();
+    private ArrayList<Post> privatePosts = new ArrayList<Post>();
+
+    //2. get and set
+
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        if(title != null && title.matches("[A-Za-z0-9 ]{5,20}"))
+            this.title = title;
+        else
+            this.title =  "------";
+    }
+
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        if(title != null && description.length() > 10 && description.length() < 200)
+            this.description = description;
+        else
+            this.description = "------";
+    }
+
+    //3. constructors
+
+    public Page()
+    {
+        setTitle("Test page");
+        setDescription("Test page description");
+    }
+    public Page(String title, String description)
+    {
+        setTitle(title);
+        setDescription(description);
+    }
+
+    //4. toString
+
+    public String toString(){
+        return title + "( " + description + "): [" + (privatePosts.size() + publicPosts.size()) + "]";
+    }
+
+}
